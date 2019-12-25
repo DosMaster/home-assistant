f85dd43dafde822e770472995645e4538fa8aa76
Hans Nilsson <hans.nilsson@zwyck.com>
2019-12-25 13:06:55 +0100

    EnOcean: Support for Eltako Switch (FT55).   use "device_class: switch_eltako"

--- homeassistant/components/enocean/switch.py
+++ homeassistant/components/enocean/switch.py
@@ -5,7 +5,7 @@ import voluptuous as vol
 
 from homeassistant.components import enocean
 from homeassistant.components.switch import PLATFORM_SCHEMA
-from homeassistant.const import CONF_ID, CONF_NAME
+from homeassistant.const import CONF_DEVICE_CLASS, CONF_ID, CONF_NAME
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import ToggleEntity
 
@@ -14,11 +14,15 @@ _LOGGER = logging.getLogger(__name__)
 CONF_CHANNEL = "channel"
 DEFAULT_NAME = "EnOcean Switch"
 
+DEVICE_CLASS_SWITCH = "switch"
+DEVICE_CLASS_SWITCH_ELTAKO = "switch_eltako"
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_ID): vol.All(cv.ensure_list, [vol.Coerce(int)]),
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_CHANNEL, default=0): cv.positive_int,
+        vol.Optional(CONF_DEVICE_CLASS, default=DEVICE_CLASS_SWITCH): cv.string,
     }
 )
 
@@ -28,8 +32,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     channel = config.get(CONF_CHANNEL)
     dev_id = config.get(CONF_ID)
     dev_name = config.get(CONF_NAME)
+    dev_class = config.get(CONF_DEVICE_CLASS)
 
-    add_entities([EnOceanSwitch(dev_id, dev_name, channel)])
+    if dev_class == DEVICE_CLASS_SWITCH:
+        add_entities([EnOceanSwitch(dev_id, dev_name, channel)])
+    elif dev_class == DEVICE_CLASS_SWITCH_ELTAKO:
+        add_entities([EnOceanSwitchEltako(dev_id, dev_name, channel)])
 
 
 class EnOceanSwitch(enocean.EnOceanDevice, ToggleEntity):
@@ -75,6 +83,82 @@ class EnOceanSwitch(enocean.EnOceanDevice, ToggleEntity):
             optional=optional,
             packet_type=0x01,
         )
+        self._on_state = False
+
+    def value_changed(self, packet):
+        """Update the internal state of the switch."""
+        if packet.data[0] == 0xA5:
+            # power meter telegram, turn on if > 10 watts
+            packet.parse_eep(0x12, 0x01)
+            if packet.parsed["DT"]["raw_value"] == 1:
+                raw_val = packet.parsed["MR"]["raw_value"]
+                divisor = packet.parsed["DIV"]["raw_value"]
+                watts = raw_val / (10 ** divisor)
+                if watts > 1:
+                    self._on_state = True
+                    self.schedule_update_ha_state()
+        elif packet.data[0] == 0xD2:
+            # actuator status telegram
+            packet.parse_eep(0x01, 0x01)
+            if packet.parsed["CMD"]["raw_value"] == 4:
+                channel = packet.parsed["IO"]["raw_value"]
+                output = packet.parsed["OV"]["raw_value"]
+                if channel == self.channel:
+                    self._on_state = output > 0
+                    self.schedule_update_ha_state()
+
+
+class EnOceanSwitchEltako(enocean.EnOceanDevice, ToggleEntity):
+    """Representation of an EnOcean switch device."""
+
+    def __init__(self, dev_id, dev_name, channel):
+        """Initialize the EnOcean switch device."""
+        super().__init__(dev_id, dev_name)
+        self._light = None
+        self._on_state = False
+        self._on_state2 = False
+        self.channel = channel
+
+    @property
+    def is_on(self):
+        """Return whether the switch is on or off."""
+        return self._on_state
+
+    @property
+    def name(self):
+        """Return the device name."""
+        return self.dev_name
+
+    def turn_on(self, **kwargs):
+        """Turn on the switch."""
+        if self.channel == 0:
+            data = 0x30
+        else:
+            data = 0x70
+        command = [0xF6, data]
+        command.extend(self.dev_id)
+        command.extend([0x00])
+        self.send_command(command, [], 0x01)
+        command = [0xF6, 0x00]
+        command.extend(self.dev_id)
+        command.extend([0x00])
+        self.send_command(command, [], 0x01)
+        self._on_state = True
+
+    def turn_off(self, **kwargs):
+        """Turn off the switch."""
+        if self.channel == 0:
+            data = 0x10
+        else:
+            data = 0x50
+        command = [0xF6, data]
+        command.extend(self.dev_id)
+        command.extend([0x00])
+        self.send_command(command, [], 0x01)
+        command = [0xF6, 0x00]
+        command.extend(self.dev_id)
+        command.extend([0x00])
+        self.send_command(command, [], 0x01)
         self._on_state = False
 
     def value_changed(self, packet):
