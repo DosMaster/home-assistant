76832fc40a6508ac2c19ec1f7f4c71829fe29a5d
Hans Nilsson <hans.nilsson@zwyck.com>
2020-01-03 17:52:35 +0100

    EnOcean: Support for Eltako FUD14 Dimmer Bugfix: Dimmer value decreased for each on/off

--- homeassistant/components/enocean/light.py
+++ homeassistant/components/enocean/light.py
@@ -1,6 +1,5 @@
 """Support for EnOcean light sources."""
 import logging
-import math
 
 import voluptuous as vol
 
@@ -20,12 +19,14 @@ CONF_SENDER_ID = "sender_id"
 
 DEFAULT_NAME = "EnOcean Light"
 SUPPORT_ENOCEAN = SUPPORT_BRIGHTNESS
+CONF_DEVICE_TYPE = "device_type"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_ID, default=[]): vol.All(cv.ensure_list, [vol.Coerce(int)]),
         vol.Required(CONF_SENDER_ID): vol.All(cv.ensure_list, [vol.Coerce(int)]),
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_DEVICE_TYPE, default="std"): cv.string,
     }
 )
 
@@ -35,19 +36,27 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     sender_id = config.get(CONF_SENDER_ID)
     dev_name = config.get(CONF_NAME)
     dev_id = config.get(CONF_ID)
+    dev_type = config.get(CONF_DEVICE_TYPE)
 
-    add_entities([EnOceanLight(sender_id, dev_id, dev_name)])
+    add_entities([EnOceanLight(sender_id, dev_id, dev_name, dev_type)])
 
 
 class EnOceanLight(enocean.EnOceanDevice, Light):
     """Representation of an EnOcean light source."""
 
-    def __init__(self, sender_id, dev_id, dev_name):
+    def __init__(self, sender_id, dev_id, dev_name, dev_type):
         """Initialize the EnOcean light source."""
         super().__init__(dev_id, dev_name)
         self._on_state = False
         self._brightness = 50
         self._sender_id = sender_id
+        self._dev_type = dev_type
+
+        if dev_type.lower() == "std" or dev_type.lower() == "fud14":
+            self.dev_type = dev_type.lower()
+        else:
+            self.dev_type = None
+            _LOGGER.warning('device_type "%s" (%s) unknown', dev_type, dev_name)
 
     @property
     def name(self):
@@ -79,7 +88,7 @@ class EnOceanLight(enocean.EnOceanDevice, Light):
         if brightness is not None:
             self._brightness = brightness
 
-        bval = math.floor(self._brightness / 256.0 * 100.0)
+        bval = round(self._brightness / 256.0 * 100.0)
         if bval == 0:
             bval = 1
         command = [0xA5, 0x02, bval, 0x01, 0x09]
@@ -90,7 +99,10 @@ class EnOceanLight(enocean.EnOceanDevice, Light):
 
     def turn_off(self, **kwargs):
         """Turn the light source off."""
-        command = [0xA5, 0x02, 0x00, 0x01, 0x09]
+        if self.dev_type == "fud14":
+            command = [0xA5, 0x02, 0x00, 0x01, 0x08]
+        else:
+            command = [0xA5, 0x02, 0x00, 0x01, 0x09]
         command.extend(self._sender_id)
         command.extend([0x00])
         self.send_command(command, [], 0x01)
@@ -104,6 +116,14 @@ class EnOceanLight(enocean.EnOceanDevice, Light):
         """
         if packet.data[0] == 0xA5 and packet.data[1] == 0x02:
             val = packet.data[2]
-            self._brightness = math.floor(val / 100.0 * 256.0)
-            self._on_state = bool(val != 0)
+            if self.dev_type == "fud14":
+                if (packet.data[4] & 0x01) == 1:
+                    self._brightness = round(val / 100.0 * 256.0)
+                    self._on_state = True
+                else:
+                    self._on_state = False
+            else:
+                self._brightness = round(val / 100.0 * 256.0)
+                self._on_state = bool(val != 0)
+
             self.schedule_update_ha_state()
